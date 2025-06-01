0d9d6580a0a59065b62c066bee3f1ff2d61fecc2
hermanni-fernandes <hermanni.fernandes@gmail.com>
2025-06-01 17:51:14 +0300

    Päivitetty master branch opettajan branchin mukaiseksi: poistettu vanhat Hilt-riippuvuudet, päivitetty Compose Compiler, sovellus käynnistyy ja buildaa onnistuneesti.

--- app/src/main/java/com/example/restaurantlistapp/RestaurantRepository.kt
+++ app/src/main/java/com/example/restaurantlistapp/RestaurantRepository.kt
@@ -2,14 +2,12 @@ package com.example.restaurantlistapp.data
 
 import com.example.restaurantlistapp.Restaurant
 import com.example.restaurantlistapp.network.RestaurantApi
-import javax.inject.Inject
+
 
 // Repository, joka vastaa tietojen hakemisesta API:sta
-class RestaurantRepository @Inject constructor(
-    private val api: RestaurantApi
-) {
+class RestaurantRepository {
     // Hakee kaikki ravintolat API:sta
     suspend fun getAllRestaurants(): List<Restaurant> {
-        return api.getRestaurants()
+        return emptyList()
     }
 }
